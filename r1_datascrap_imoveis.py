3f3e31dd7c64450cc76e5e93948b8a1fd12cd2ea
larif.q@live.com <larif.q@live.com>
2025-03-15 19:54:08 -0300

    Adicionando arquivos no repositório

---
+++ r1_datascrap_imoveis.py
@@ -0,0 +1,103 @@
+import requests
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from re import search
+from time import sleep
+
+class Extrator_de_Dados():
+
+    def __init__(self):
+        self.driver = False
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+    def identifica_anunciante_do_url(self, url:str) -> str:
+        return search("www.(.*?).com", url).group(1)
+
+    def extrair_urls_desta_pesquisa(self, url_da_pesquisa:str) -> list:
+        """
+        Entra com um link da pesquisa, exemplo, URL da pesquisa realizado em São Paulo com 2 dorm com preço máximo de R$ 2.000
+        Retorna uma lista de URL dos imóveis encontrados.
+        Retorna vazio caso nao encontrar
+        """
+        lista_de_links = []
+        html_response = requests.get(url_da_pesquisa, headers=self.headers).text
+        imobiliaria = self.identifica_anunciante_do_url(url_da_pesquisa)
+        match imobiliaria:
+
+            case "chavesnamao":
+                SEPARADOR_DOS_LINKS_NO_HTML = '<script type="application/ld+json">'
+                html_separados:list = html_response.split(SEPARADOR_DOS_LINKS_NO_HTML)
+                html_separados.pop(0)
+                
+                for item in html_separados:
+                    json_in_html = item.split("</script>")[0]
+                    try:
+                        especific_url = json.loads(json_in_html)
+                    except json.JSONDecodeError as e:
+                        char_pos = e.pos
+                        json_string_truncada = json_in_html[:char_pos]  # Cortar a string até o ponto do erro
+                        especific_url = json.loads(json_string_truncada)
+                    if "object" in especific_url:
+                        especific_url = especific_url["object"]["url"]
+                        lista_de_links.append(especific_url)
+        return lista_de_links
+
+    def extrair_dados_imobiliarios_desta_url(self, url_do_imovel:str) -> dict:
+        if not self.driver:
+            chrome_options = Options()
+            chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+            chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            chrome_options.add_experimental_option("useAutomationExtension", False)
+            self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        self.driver.get(url_do_imovel)
+        imobiliaria = self.identifica_anunciante_do_url(url_do_imovel)
+        dict_dados_obtidos = {'url':url_do_imovel,
+                    'imobiliaria':imobiliaria}
+
+        match imobiliaria:
+            case "chavesnamao":
+                XP_BOTAO_EXPANDIR_DESCRICAO = "/html/body/main/article/section[2]/div/div[1]/div/span/svg"
+                try:
+                    elemento = self.driver.find_element(By.XPATH, XP_BOTAO_EXPANDIR_DESCRICAO)  # Tenta localizar o elemento
+                    elemento.click()  # Se encontrado, clica nele
+                except NoSuchElementException:
+                    pass
+                XP_PATHS = {
+                "ALUGUEL": "/html/body/main/article/section[2]/div/table/tbody/tr[1]/td[1]/p[2]/b",
+                "CONDOMINIO": "/html/body/main/article/section[2]/div/table/tbody/tr[2]/td[2]/p",
+                "IPTU": "/html/body/main/article/section[2]/div/table/tbody/tr[3]/td[2]/p",
+                "ALUGUEL_CONDOMINIO": "/html/body/main/article/section[2]/div/table/tbody/tr[4]/td[2]/p",
+                "ENDERECO": "/html/body/main/article/section[2]/div/address/span/h2/b",
+                "TITULO": "/html/body/main/article/section[2]/div/span/h1",
+                "AREA": "/html/body/main/article/section[2]/div/ul/li[1]/p/b",
+                #"AREA_UTIL_TOTAL": "/html/body/main/article/section[2]/div/ul/li[1]/p/b", # idem ao de cima
+                "QUARTOS": "/html/body/main/article/section[2]/div/ul/li[2]/p/b",
+                "SUITES": "/html/body/main/article/section[2]/div/ul/li[3]/p/b",
+                "GARAGENS": "/html/body/main/article/section[2]/div/ul/li[3]/p/b",
+                "BANHEIROS": "/html/body/main/article/section[2]/div/ul/li[4]/p/b",
+                "ATUALIZACAO_REF": "/html/body/main/article/section[2]/div/div[1]/div/p[1]",
+                "DESCRICAO": "/html/body/main/article/section[2]/div/div[1]/div/p[2]",
+                "ANUNCIANTE": "/html/body/main/article/section[2]/aside/div[2]/span/span[2]/a/span/h2/b",
+                "ESPACO_PRIVATIVO": "/html/body/main/article/section[2]/div/div[2]/span[1]/ul",
+                "AREA_COMUM": "/html/body/main/article/section[2]/div/div[2]/span[2]/ul"
+                }
+
+        for nome, xpath in XP_PATHS.items():
+            try:
+                elemento = self.driver.find_element(By.XPATH, xpath)
+                valor = elemento.text.strip()
+                dict_dados_obtidos[nome] = valor
+            except:
+                pass
+
+        return dict_dados_obtidos
+    
+    
+    def exit(self):
+        self.driver.quit()  # Fechar o navegador
+            
